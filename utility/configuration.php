f44a6c94c71e3a02ac558ccdef708306bee20968
Kelvin <kelvingraddick@gmail.com>
2018-04-17 01:55:37 -0400

    Configuration file updates

--- utility/configuration.php
+++ utility/configuration.php
@@ -1,8 +1,11 @@
 <?php
 
-	$database_host = "localhost";
-	$database_username = "application";
-	$database_password = "password";
-	$database_name = "database";
+    $site_name = "KG The Maker";
 
+	$database_host = "localhost";
+	$database_username = "username";
+	$database_password = "password";
+	$database_name = "name";
+
+    putenv("SENDGRID_API_KEY=key");
 ?>
