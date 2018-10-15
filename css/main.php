059557c80965be8aa71d180a451c0d3cba36a567
Kelvin <kgraddick@wavelinkllc.com>
2018-10-15 03:48:51 -0400

    Favicon implementation

--- css/main.php
+++ css/main.php
@@ -3,3 +3,10 @@
 <link href='https://<?php echo $_SERVER['SERVER_NAME']; ?>/css/main.css' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/x-icon">
 <link rel="icon" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon-16x16.png">
+<link rel="manifest" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/site.webmanifest">
+<link rel="mask-icon" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/safari-pinned-tab.svg" color="#3e3e3f">
+<meta name="msapplication-TileColor" content="#eed67a">
+<meta name="theme-color" content="#eed67a">
