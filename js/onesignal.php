409c24144228456601be2ebef5649f14e20ee509
Kelvin <kgraddick@wavelinkllc.com>
2018-10-15 02:43:18 -0400

    removing console error logging for OneSignal script

--- js/onesignal.php
+++ js/onesignal.php
@@ -2,7 +2,6 @@
 <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"></script>
 <script>
   var OneSignal = window.OneSignal || [];
-  console.error(OneSignal);
   OneSignal.push(function() {
     OneSignal.init({
       appId: "appId",
