be1dfde5c672e22625dcf3694b1905a170880d41
Kelvin <kelvingraddick@kg.codes>
2023-11-30 22:08:35 -0500

    Update jquery

--- js/main.php
+++ js/main.php
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
 <script src="https://<?php echo $_SERVER['SERVER_NAME']; ?>/js/main.js"></script>
 <script>
